fd609c51c767a594f71838d78fe2a9b0f22ed635
Lộc Văn <loccosi3005@gmail.com>
2023-04-04 08:28:46 +0700

    add

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express();
 var bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
-var AccountModel=require('./sanpham');
+var AccountModel = require('./sanpham');
 var methodOverride = require('method-override')
-const { mongooestoObject,mutipleMongooesToObject } = require('./util/monggo');
+const { mongooestoObject, mutipleMongooesToObject } = require('./util/monggo');
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -12,25 +12,25 @@ app.engine('.hbs', expressHbs.engine({ extname: "hbs", defaultLayout: "main" }))
 app.set('view engine', '.hbs');
 app.set('views', './');
 
-app.get('/',(req,res,next)=>{
+app.get('/', (req, res, next) => {
     AccountModel.find({}).then(sanpham => {
-        res.render('home', {sanpham:sanpham.map(item=>item.toObject())})
+        res.render('home', { sanpham: sanpham.map(item => item.toObject()) })
     }).catch(next);
 })
-app.get('/themsanpham',(req,res)=>{
+app.get('/themsanpham', (req, res) => {
     res.render('themsanpham')
 })
-app.post('/themsanpham',(req,res)=>{
-    var ten=req.body.tensp;
-    var gia=req.body.giasp;
-    var soluong =req.body.soluong;
-    var khuyenmai=req.body.khuyenmai;
+app.post('/themsanpham', (req, res) => {
+    var ten = req.body.tensp;
+    var gia = req.body.giasp;
+    var soluong = req.body.soluong;
+    var khuyenmai = req.body.khuyenmai;
     AccountModel.insertMany({
-        tensp:ten,
-        giasp:gia,
-        soluong:soluong,
-        khuyenmai:khuyenmai,
-    }).then(data=>{
+        tensp: ten,
+        giasp: gia,
+        soluong: soluong,
+        khuyenmai: khuyenmai,
+    }).then(data => {
         res.redirect('/')
     })
 
@@ -39,7 +39,6 @@ app.get("/:id/edit", async (req, res) => {
     AccountModel.findById(req.params.id).then(sanpham => res.render('edit', { sanpham: sanpham.toObject() }))
 });
 app.put("/:id", async (req, res) => {
-    // res.json(req.body)
     AccountModel.updateOne({ _id: req.params.id }, req.body).then(() => res.redirect('/'))
 });
 app.get('/:id', async (req, res) => {
